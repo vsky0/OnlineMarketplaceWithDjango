81daf02a14e5f3e7fb1d24b721ea3bf84431df38
Velpula Sai Kiran Yadav <saikiranyadavvelpula.001@gmail.com>
2024-06-19 14:12:07 +0530

    upload to remote

---
+++ item/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+
+from .models import Item
+
+INPUT_CLASSES= 'w-full py-4 px-6 rounded-xl border'
+# Form to add item details
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('category', 'name', 'description', 'price', 'image',)
+        widgets = {
+            'category': forms.Select(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'price': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+        }
+
+# Form to Edit the item details
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('name', 'description', 'price', 'image', 'is_sold')
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'price': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+        }
---
+++ item/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'item' # here app_name is name space
+
+urlpatterns = [
+    path('', views.browse_items, name='browse_items'),
+    path('new/', views.new, name='new'),
+    path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/delete', views.delete, name='delete'),
+    path('<int:pk>/edit', views.edit, name='edit'),
+]
